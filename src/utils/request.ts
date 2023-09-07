f8491cef49380f93a027f2872899a7fc9b54c0de
FunnySaltyFish <3036548996@qq.com>
2023-09-08 00:13:09 +0800

    优化未登录时弹窗体验 & 增加自动跳转登录页

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -7,14 +7,16 @@
  */
 import axios from 'axios';
 import store from './store';
+// import { useRouter, useRoute } from 'vue-router';
+import router from "../router/index";
 
 const http = axios.create();
 
 // 添加请求拦截器
 http.interceptors.request.use(
-  (config:any) => {
-    if(config.url[0]=='/')config.url=store.api_url+config.url;
-    config.headers['fake-cookie']=store.fake_cookie;
+  (config: any) => {
+    if (config.url[0] == '/') config.url = store.api_url + config.url;
+    config.headers['fake-cookie'] = store.fake_cookie;
     return config;
   },
   (err) => {
@@ -25,22 +27,27 @@ http.interceptors.request.use(
 // 添加响应拦截器
 http.interceptors.response.use(
   (res) => {
-    if(res.data.msg)window.$message.success(res.data.msg);
+    if (res.data.msg) window.$message.success(res.data.msg);
     return res;
   },
   (err) => {
-    if(err.request.status==500){
-      if(err.response.data.msg)window.$message.error(err.response.data.msg);
+    if (err.request.status == 500) {
+      if (err.response.data.msg) window.$message.error(err.response.data.msg);
       else window.$message.error('出错了Orz');
     }
-    else if(err.request.status==401){
-        if(err.response.data.msg)window.$message.error(err.response.data.msg);
-        else window.$message.error('请先登录awa');
+    else if (err.request.status == 401) {
+      // 如果当前已经处于登录页面，什么也不做
+      const route = router.currentRoute.value;
+      if (route.name != "login") {
+        window.$message.error(err.response.data.msg || '请先登录awa');
+        // 允许在主页看一看，否则的话跳转到登录页面
+        if (route.path != "/") router.push("/login")
       }
-      else if(err.request.status==400){
-        if(err.response.data.msg)window.$message.error(err.response.data.msg);
-        else window.$message.error('请检查请求参数awa');
-      }
+    }
+    else if (err.request.status == 400) {
+      if (err.response.data.msg) window.$message.error(err.response.data.msg);
+      else window.$message.error('请检查请求参数awa');
+    }
     return Promise.reject(err);
   }
 );
