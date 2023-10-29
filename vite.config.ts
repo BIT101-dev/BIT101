7860d6025774f91850984b5984e6f5db94ab0761
flwfdd <1525876733@qq.com>
2023-10-29 17:16:56 +0800

    🤩添加PWA支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: flwfdd
  * @Date: 2022-05-28 00:01:07
- * @LastEditTime: 2023-09-17 15:52:25
+ * @LastEditTime: 2023-10-29 17:13:09
  * @Description: 
  * _(:з」∠)_
  */
@@ -10,14 +10,54 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import { resolve } from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Components({
-    resolvers: [NaiveUiResolver()]
-  })],
+      resolvers: [NaiveUiResolver()]
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      },
+      includeAssets: ['favicon.ico', 'pwa-512x512.png'],
+      manifest: {
+        name: 'BIT101',
+        short_name: 'BIT101',
+        description: 'BIT101校园社区',
+        theme_color: '#FF9A57',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, "src")
