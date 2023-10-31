a94edd24d4d2b9c5612493668a4f0126a59b0809
Dustin Jiang <dustinjiang@outlook.com>
2023-10-31 10:47:07 +0800

    ♿添加快速跳转评论区FAB

--- src/views/GalleryShow.vue
+++ src/views/GalleryShow.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: flwfdd
  * @Date: 2023-10-20 17:39:36
- * @LastEditTime: 2023-10-31 09:52:55
+ * @LastEditTime: 2023-10-31 10:40:37
  * @Description: _(:з」∠)_
 -->
 <script setup lang="ts">
@@ -9,7 +9,7 @@ import http from '@/utils/request';
 import { onActivated, onDeactivated, onMounted, onUnmounted, ref, watch } from 'vue';
 import { onBeforeRouteLeave, onBeforeRouteUpdate, useRoute, useRouter } from 'vue-router';
 import { FormatTime, Clip, setTitle, OpenLink, Share } from '@/utils/tools';
-import { EditOutlined, ThumbUpOutlined, ThumbUpFilled, ShareOutlined, ErrorOutlined, DeleteOutlined, FeedbackOutlined } from '@vicons/material';
+import { AddCommentOutlined, EditOutlined, ThumbUpOutlined, ThumbUpFilled, ShareOutlined, ErrorOutlined, DeleteOutlined, FeedbackOutlined } from '@vicons/material';
 import Comment from '@/components/Comment.vue';
 import { Poster } from '@/utils/types';
 import Avatar from '@/components/Avatar.vue';
@@ -17,6 +17,7 @@ import RenderLink from '@/components/RenderLink.vue';
 
 //Poster数据
 const poster = ref({} as Poster)
+const actions = ref<HTMLDivElement>()
 
 //加载Poster
 function LoadPoster() {
@@ -54,6 +55,14 @@ function SharePoster() {
 function DeletePoster() {
   http.delete("/posters/" + poster.value.id).then(() => {
     router.go(-1);
+  })
+}
+
+// 滚动到actions
+function ScrollToActions() {
+  console.log(actions)
+  actions.value!.scrollIntoView({
+    behavior: "smooth"
   })
 }
 
@@ -143,51 +152,62 @@ router.afterEach(async (to, from) => {
       最后编辑于 {{ FormatTime(poster.edit_time) }}
     </p>
 
-    <n-space style="margin-top:4px" justify="end">
-      <n-button v-if="poster.own" @click="OpenLink('/gallery/edit/' + poster.id)" icon-placement="right" ghost>
-        <template #icon>
-          <n-icon :component="EditOutlined" />
-        </template>
-        编辑
-      </n-button>
+    <!-- 套一个div 让ref类型是HTMLDivElement 方便用ScrollIntoView() -->
+    <div ref="actions">
+      <n-space style="margin-top:4px" justify="end">
+        <n-button v-if="poster.own" @click="OpenLink('/gallery/edit/' + poster.id)" icon-placement="right" ghost>
+          <template #icon>
+            <n-icon :component="EditOutlined" />
+          </template>
+          编辑
+        </n-button>
 
-      <n-popconfirm v-if="poster.own" @positive-click="DeletePoster" :show-icon="false" positive-text="确定"
-        negative-text="取消">
-        <template #trigger>
-          <n-button icon-placement="right" ghost>
-            <template #icon>
-              <n-icon :component="DeleteOutlined" />
-            </template>
-            删除
-          </n-button>
-        </template>
-        汝真断舍离耶？
-      </n-popconfirm>
+        <n-popconfirm v-if="poster.own" @positive-click="DeletePoster" :show-icon="false" positive-text="确定"
+          negative-text="取消">
+          <template #trigger>
+            <n-button icon-placement="right" ghost>
+              <template #icon>
+                <n-icon :component="DeleteOutlined" />
+              </template>
+              删除
+            </n-button>
+          </template>
+          汝真断舍离耶？
+        </n-popconfirm>
 
-      <n-button @click="router.push('/report/poster' + poster.id)" icon-placement="right" ghost>
-        <template #icon>
-          <n-icon :component="FeedbackOutlined" />
-        </template>
-        举报
-      </n-button>
+        <n-button @click="router.push('/report/poster' + poster.id)" icon-placement="right" ghost>
+          <template #icon>
+            <n-icon :component="FeedbackOutlined" />
+          </template>
+          举报
+        </n-button>
 
-      <n-button @click="SharePoster" icon-placement="right" ghost>
-        <template #icon>
-          <n-icon :component="ShareOutlined" />
-        </template>
-        分享
-      </n-button>
+        <n-button @click="SharePoster" icon-placement="right" ghost>
+          <template #icon>
+            <n-icon :component="ShareOutlined" />
+          </template>
+          分享
+        </n-button>
 
-      <n-button @click="Like" icon-placement="right" color="#fb7299" :ghost="!poster.like" :loading="like_loading"
-        :disabled="like_loading">
-        <template #icon>
-          <n-icon :component="poster.like ? ThumbUpFilled : ThumbUpOutlined" />
-        </template>
-        {{ poster.like_num }}赞同
-      </n-button>
+        <n-button @click="Like" icon-placement="right" color="#fb7299" :ghost="!poster.like" :loading="like_loading"
+          :disabled="like_loading">
+          <template #icon>
+            <n-icon :component="poster.like ? ThumbUpFilled : ThumbUpOutlined" />
+          </template>
+          {{ poster.like_num }}赞同
+        </n-button>
 
-    </n-space>
+      </n-space>
+    </div>
     <n-divider style="color:#809BA8;font-size:14px;">现有{{ poster.comment_num }}条评论</n-divider>
     <Comment :obj='"poster" + poster.id'></Comment>
+    <n-space vertical style="position:fixed;right:4.2vw;bottom:4.2vw;">
+      <n-button @click="ScrollToActions()" circle :bordered="false"
+        style="background-color:#FFFA;width:50px;height: 50px;box-shadow: 0 0 11px #ccc;">
+        <template #icon>
+          <n-icon :component="AddCommentOutlined" size="24" />
+        </template>
+      </n-button>
+    </n-space>
   </div>
 </template>
